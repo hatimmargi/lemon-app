3f1425f6ffd58064b08a613dba4a1177df1dffc7
Hatim Margi <hatimmargi4@gmail.com>
2024-04-29 13:40:33 +0400

    finish responsive css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import "./App.css";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Main from "./components/Main";
-import Cards from "./components/Cards";
-import Testimonials from "./components/Testimonials";
-import About from "./components/About";
-import Footer from "./components/Footer";
+import HomePage from "./components/HomePage";
 function App() {
   return (
     <>
-      <Header>
-        <Nav />
-      </Header>
-      <Main />
-      <Cards/>
-      <Testimonials/>
-      <About/>
-      <Footer />
+  <HomePage/>
     </>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,22 @@
+import Header from "./Header";
+import Nav from "./Nav";
+import Main from "./Main";
+import Cards from "./Cards";
+import Testimonials from "./Testimonials";
+import About from "./About";
+import Footer from "./Footer";
+function HomePage () {
+    return (
+<>
+<Header>
+<Nav />
+</Header>
+<Main />
+<Cards/>
+<Testimonials/>
+<About/>
+<Footer />
+</>
+)
+}
+export default HomePage

c245a3a2c5dd5b73688663eee593ac8a4aed7117
Hatim Margi <hatimmargi4@gmail.com>
2024-04-29 12:05:53 +0400

    finishing desktop css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,20 @@
 import logo from "../imgs/little-lemon-logo.png"
 import facebookIcon from "../svgs/bxl-facebook.svg"
+import twitterIcon from "../svgs/bxl-twitter.svg"
+import instagramIcon from "../svgs/instagram-logo.svg"
 function Footer () {
     return (
-        <>
-        <section>
-        <img src={logo} alt="little lemon logo"/>
-        <ul>
-        <li><a href="#"><img src={facebookIcon} alt="fb icon"/></a></li>
-        <li><a href="#"><img src={facebookIcon} alt="fb icon"/></a></li>
-        <li><a href="#"><img src={facebookIcon} alt="fb icon"/></a></li>
-        <li><a href="#"><img src={facebookIcon} alt="fb icon"/></a></li>
+        <footer>
+        <section className="footer-section">
+        <img className="logo" src={logo} alt="little lemon logo"/>
+        <ul className="icons">
+        <li><a href="#"><img src={facebookIcon} alt="facebook icon"/></a></li>
+        <li><a href="#"><img src={twitterIcon} alt="twitter icon"/></a></li>
+        <li><a href="#"><img src={instagramIcon} alt="inatagram icon"/></a></li>
         </ul>
         </section>
-        <section>
-        <ul>
+        <section className="footer-section">
+        <ul className="links">
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Menu</a></li>
@@ -22,12 +23,12 @@ function Footer () {
         <li><a href="#">Login</a></li>
         </ul>
         </section>
-        <section>
+        <section className="footer-section">
         <li><p>Phone: 123456789</p></li>
         <li><p>Location: Chicago, USA</p></li>
         <li><p>Email: littlelemon@gmail.com</p></li>
         </section>
-        </>
+        </footer>
 )
 }
 export default Footer
